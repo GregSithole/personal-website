7f32b4a7451be80cdeda73a5ce0a487dff2b7486
Greg Sithole <gregsithole@hotmail.co.za>
2020-10-27 01:29:31 +0200

    Removed Default App Module & Component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeModule } from 'src/app/home/home.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./home/home-routing.module').then(m => m.HomeRoutingModule),
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    HomeModule
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }

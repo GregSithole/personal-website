22585b1c0f3635c7e8de1d65fad1a460bd282be0
Greg Sithole <gregsithole@hotmail.co.za>
2020-10-28 14:45:48 +0200

    Added all Components to Home Module

--- frontend/src/app/home/home.module.ts
+++ frontend/src/app/home/home.module.ts
@@ -3,11 +3,18 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeRoutingModule } from 'src/app/home/home-routing.module';
+import { IntroductionComponent } from './components/introduction/introduction.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent, HeaderComponent],
+  declarations: [HomeComponent, HeaderComponent, IntroductionComponent, AboutMeComponent, SkillsComponent, ExperienceComponent, ProjectsComponent, BlogComponent, ContactComponent],
   imports: [
     CommonModule,
     HomeRoutingModule

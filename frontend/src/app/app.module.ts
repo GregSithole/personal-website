a9a5a6dfbd860a71e85e419921b58a99eed31e87
Greg Sithole <gregsithole@hotmail.co.za>
2021-04-28 23:14:20 +0200

    Added Components to App Module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { BlogComponent } from './components/blog/blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContactComponent,
+    AboutMeComponent,
+    ProjectsComponent,
+    ExperienceComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule
